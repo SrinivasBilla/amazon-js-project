4392225ba1cdbf95284e459f8591eb95bdca1391
Srinivas Billa <srinivasbilla12@gmail.com>
2024-08-06 09:13:47 -0400

    use module to move code into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-
+import { cart, addToCart } from '../data/cart.js';
+import { products } from '../data/products.js';
 
 let productsHTML = '';
 
@@ -27,7 +28,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -56,29 +57,20 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
-    let matchingItem;
 
-    cart.forEach((item) => {
-      if(productId === item.productId){
-        matchingItem = item;
-      }
-    });
-
-      if(matchingItem) {
-        matchingItem.quantity += 1;
-      }else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-      let cartQuantity = 0;
+function updateCartQuantity() {
+  let cartQuantity = 0;
       cart.forEach((item) => {
         cartQuantity += item.quantity;
       });
       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    addToCart(productId);
+    updateCartQuantity();
+      
   });
 });
