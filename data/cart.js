0250333bee728f507c910f59576e384e39742aaa
Srinivas Billa <srinivasbilla12@gmail.com>
2024-08-08 13:49:04 -0400

    Make delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -5,11 +5,13 @@ if (!cart){
 cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: "1"
   },
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: "2"
   }
 ];
 }
@@ -37,7 +39,8 @@ export function addToCart(productId) {
       }else {
         cart.push({
           productId: productId,
-          quantity: quantity
+          quantity: quantity,
+          deliveryOptionId: '1'
         });
       }
       saveToStorage();
@@ -71,4 +74,16 @@ export function updateQuantity(productId, newQuantity) {
   });
   matchingItem.quantity = newQuantity;
   saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
+  
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,10 @@
-import { calculateCartQuantity, cart, removeFromCart, updateQuantity } from "../data/cart.js";
+import { calculateCartQuantity, cart, removeFromCart, updateDeliveryOption, updateQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import  dayjs  from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+import { deliveryOptions } from "../data/deliveryOption.js";
+
+const today = dayjs();
 
 let cartSummaryHTML = '';
 
@@ -12,10 +16,27 @@ cart.forEach((cartItem) => {
       matchingItem = product;
     }
   });
+  
+  const deliveryOptionId = cartItem.deliveryOptionId;
+  let deliveryOption ;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+  const today = dayjs();
+    const deliveryDate = today.add(
+      deliveryOption.deliveryDays,
+      'days'
+    );
+    const dateString = deliveryDate.format(
+      'dddd, MMMM D'
+    );
   cartSummaryHTML += `
   <div class="cart-item-container js-cart-item-container-${matchingItem.id}">
             <div class="delivery-date">
-              Delivery date: Wednesday, June 15
+              Delivery date: ${dateString}
             </div>
 
             <div class="cart-item-details-grid">
@@ -48,49 +69,46 @@ cart.forEach((cartItem) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-
-                <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input"
-                    name="delivery-option-${matchingItem.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" checked class="delivery-option-input"
-                    name="delivery-option-${matchingItem.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input"
-                    name="delivery-option-${matchingItem.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+                ${deliveryOptionsHTMl(matchingItem, cartItem)}
               </div>
+
             </div>
           </div>
+        </div>
+        </div>
   `;
 
 });
+
+function deliveryOptionsHTMl(matchingItem, cartItem) {
+  let html = "";
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+    const dateString = deliveryDate.format('dddd, MMMM, D');
+    const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} - `;
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+    html += 
+          ` <div class="delivery-option js-delivery-option"
+                data-product-id="${matchingItem.id}"
+                data-delivery-option-id="${deliveryOption.id}">
+                        <input type="radio" 
+                        ${isChecked ? 'checked' : ''} class="delivery-option-input"
+                          name="delivery-option-${matchingItem.id}">
+                        <div>
+                          <div class="delivery-option-date">
+                            ${dateString}
+                          </div>
+                          <div class="delivery-option-price">
+                            ${priceString}
+                          </div>
+                        </div>
+                        </div>`
+
+  });
+  return html;
+}
 
 
 document.querySelector('.js-cart-summary').innerHTML = cartSummaryHTML;
@@ -147,3 +165,10 @@ document.querySelectorAll(".js-save-quantity-link").forEach((link) => {
 
 });
 
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
+  });
+
+})
