2aed2d07d15d1d51c4609aaf4470cb4a6db918cc
Srinivas Billa <srinivasbilla12@gmail.com>
2024-08-12 11:04:51 -0400

    save data and generate HTML for payment summery on checkout.js page

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,28 +1,18 @@
 import { calculateCartQuantity, cart, removeFromCart, updateDeliveryOption, updateQuantity } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import  dayjs  from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryOption.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOption.js";
 
 export function renderOrderSummery() {
     let cartSummaryHTML = '';
     cart.forEach((cartItem) => {
       const productId = cartItem.productId;
-      let matchingItem;
-      products.forEach((product) => {
-        if (product.id === productId) {
-          matchingItem = product;
-        }
-      });
+      const matchingItem = getProduct(productId);
+      
       
       const deliveryOptionId = cartItem.deliveryOptionId;
-      let deliveryOption ;
-
-      deliveryOptions.forEach((option) => {
-        if (option.id === deliveryOptionId) {
-          deliveryOption = option;
-        }
-      });
+      const deliveryOption = getDeliveryOption(deliveryOptionId);
         const today = dayjs();
           const deliveryDate = today.add(
             deliveryOption.deliveryDays,
