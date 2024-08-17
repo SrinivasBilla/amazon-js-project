7cd244cb4adeb3cf92354d1e1b2b5fc8d4232279
SrinivasBilla <srinivasbilla12@gmail.com>
2024-08-17 22:36:09 +0530

    Learn Promises covert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,13 +5,32 @@ import '../data/cart-class.js';
 //import '../data/car.js';
 //import '../data/backend-practice.js';
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then((values) => {
   renderOrderSummary();
-renderPaymentSummary();
-  
-})
+  renderPaymentSummary();
+});
 renderCheckOutHeader();
 
+/*
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  
+})
+
+*/
 
 
